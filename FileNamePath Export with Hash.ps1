595ff70b96edde8d9425fc16705e6f79f92c08f1
cameronstewart <info@cameronstewart.com.au>
2018-05-10 17:03:49 +1000

    Add files via upload

---
+++ FileNamePath Export with Hash.ps1
@@ -0,0 +1,41 @@
+﻿$SourcePath = "g:\is"
+
+$DestinationCSVPath = "h:\IS.csv" #Destination for Temp CSV File
+$CSVColumnOrder = 'Path','Directory','Name','CreationTime','LastAccessTime','LastWriteTime','Extension','BaseName','B','MD5'#,'SHA1' #Order in which columns in CSV Output are ordered
+ 
+
+$SourcePathFileOutput = Get-ChildItem $SourcePath -File -Recurse
+
+$HashOutput = ForEach ($file in $SourcePathFileOutput){
+
+Write-Output (New-Object -TypeName PSCustomObject -Property @{
+
+Path = $file.FullName
+
+Directory = $File.DirectoryName
+
+Name = $File.Name
+
+CreationTime = $File.CreationTime
+
+LastAccessTime = $File.LastAccessTime
+
+LastWriteTime = $File.LastWriteTime
+
+Extension = $File.Extension
+
+BaseName = $File.BaseName
+
+B = $File.Length
+
+MD5 = Get-FileHash $file.FullName -Algorithm MD5 | Select-Object -ExpandProperty Hash
+
+#SHA1 = Get-FileHash $file.FullName -Algorithm SHA1 | Select-Object -ExpandProperty Hash
+
+}) | Select-Object $CSVColumnOrder
+
+}
+
+ 
+
+$HashOutput | Export-Csv -NoTypeInformation -Path $DestinationCSVPath
