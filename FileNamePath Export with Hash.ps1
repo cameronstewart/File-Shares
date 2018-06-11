0fbf873e44101f3f4ebba7a63ab629ffa54a3f6d
cameronstewart <info@cameronstewart.com.au>
2018-06-11 20:24:05 +1000

    FileName and Folder Name with Path and Attributes
    Extended the data that is returned to more fields

--- FileNamePath Export with Hash.ps1
+++ FileNamePath Export with Hash.ps1
@@ -1,10 +1,15 @@
-﻿$SourcePath = "g:\is"
+#Set-ExecutionPolicy Unrestricted
+$SourcePath = "E:\Music"
 
-$DestinationCSVPath = "h:\IS.csv" #Destination for Temp CSV File
-$CSVColumnOrder = 'Path','Directory','Name','CreationTime','LastAccessTime','LastWriteTime','Extension','BaseName','B','MD5'#,'SHA1' #Order in which columns in CSV Output are ordered
+$DestinationCSVPath = "e:\folders4.csv" #Destination for Temp CSV File
+$CSVColumnOrder = 'Path', 'IsDIR', 'Directory', 'Parent', 'Name', 'CreationTime', 'LastAccessTime', 'LastWriteTime', 'Extension', 'BaseName', 'B', 'Root', 'IsReadOnly', 'Attributes', 'Owner', 'AccessToString', 'Group'#, #'MD5', #'SHA1' #Order in which columns in CSV Output are ordered
  
+#FOLDERS ONLY
+#$SourcePathFileOutput = Get-ChildItem $SourcePath -Recurse  | where {$_.PSIsContainer} 
 
-$SourcePathFileOutput = Get-ChildItem $SourcePath -File -Recurse
+#FILES AND FOLDERS
+$SourcePathFileOutput = Get-ChildItem $SourcePath -Recurse 
+
 
 $HashOutput = ForEach ($file in $SourcePathFileOutput){
 
@@ -12,7 +17,11 @@ Write-Output (New-Object -TypeName PSCustomObject -Property @{
 
 Path = $file.FullName
 
+IsDIR = $file.PSIsContainer
+
 Directory = $File.DirectoryName
+
+Parent = $file.Parent
 
 Name = $File.Name
 
@@ -28,9 +37,22 @@ BaseName = $File.BaseName
 
 B = $File.Length
 
-MD5 = Get-FileHash $file.FullName -Algorithm MD5 | Select-Object -ExpandProperty Hash
+Root = $file.Root
+
+IsReadOnly = $file.IsReadOnly
+
+Attributes = $file.Attributes
+
+Owner = $acl.owner
+
+AccessToString = $acl.accesstostring
+
+Group = $acl.group
+
+#MD5 = Get-FileHash $file.FullName -Algorithm MD5 | Select-Object -ExpandProperty Hash
 
 #SHA1 = Get-FileHash $file.FullName -Algorithm SHA1 | Select-Object -ExpandProperty Hash
+
 
 }) | Select-Object $CSVColumnOrder
 
